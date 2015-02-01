5b5a4be75de19410b036f3a1d7611b4d312d3c76
xurbanic <374347@mail.muni.cz>
2015-02-01 17:45:04 +0100

    minor changes

--- lib/dao/PkgDao.php
+++ lib/dao/PkgDao.php
@@ -42,7 +42,11 @@ class PkgDao {
    * Stores the pkg in the DB
    */
   public function create(Pkg &$pkg) {
-
+    $this->db->query(
+        "insert into Pkg set
+          name='".$this->db->escape($pkg->getName())."',
+          version='".$this->db->escape($pkg->getVersion())."',
+          release='".$this->db->escape($pkg->getRelease())."'");
     
     # Set the newly assigned id
     $pkg->setId($this->db->getLastInsertedId());
--- lib/modules/gui/www/test.php
+++ lib/modules/gui/www/test.php
@@ -3,15 +3,15 @@ include(realpath(dirname(__FILE__)) . '/../../../model/Pkg.php');
 include(realpath(dirname(__FILE__)) . '/../../../dao/PkgDao.php');
 include(realpath(dirname(__FILE__)) . '/../../../managers/DbManager.php');
 
-$pkg = new Pkg();
-$dbManager = new DbManager();
+//$pkg = new Pkg();
+//$dbManager = new DbManager();
 
-$pkg->setName("test_pkg");
-$pkg->setRelease("release123");
-$pkg->setVersion("1.2.3");
+//$pkg->setName("test_pkg");
+//$pkg->setRelease("release123");
+//$pkg->setVersion("1.2.3");
 
-$pkgDao = new PkgDao($dbManager);
+//$pkgDao = new PkgDao($dbManager);
 
-$pkgDao->create($pkg);
+//$pkgDao->create($pkg);
 echo "done";
 ?>
