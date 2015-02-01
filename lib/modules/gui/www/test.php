303caf06904f43b0f0d6eef08f5d8a8fe0b6cde1
xurbanic <374347@mail.muni.cz>
2015-02-01 17:53:48 +0100

    minor changes

--- lib/modules/gui/www/test.php
+++ lib/modules/gui/www/test.php
@@ -2,16 +2,20 @@
 include(realpath(dirname(__FILE__)) . '/../../../model/Pkg.php');
 include(realpath(dirname(__FILE__)) . '/../../../dao/PkgDao.php');
 include(realpath(dirname(__FILE__)) . '/../../../managers/DbManager.php');
+include(realpath(dirname(__FILE__)) . '/../../../common/Pakiti.php');
 
+$pakiti = new Pakiti();
 $pkg = new Pkg();
-$dbManager = new DbManager();
+
+$pakiti->init();
+$pakiti->checkDBVersion();
 
 $pkg->setName("test_pkg");
 $pkg->setRelease("release123");
 $pkg->setVersion("1.2.3");
 
-//$pkgDao = new PkgDao($dbManager);
+$pkgDao = new PkgDao($pakiti->getManager("DbManager"));
 
-//$pkgDao->create($pkg);
+$pkgDao->create($pkg);
 echo "done";
 ?>
