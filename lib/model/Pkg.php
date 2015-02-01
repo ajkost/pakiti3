94541a173105f0cf9b92d4eba1593d09a55b14d9
xurbanic <374347@mail.muni.cz>
2015-02-01 17:41:02 +0100

    Pkg.php minor changes in create function PkgDao.php minor changes tesp.php creation

--- lib/model/Pkg.php
+++ lib/model/Pkg.php
@@ -7,6 +7,8 @@
 class Pkg {
   private $_id;
   private $_name;
+  private $_version;
+  private $_release;
 
   public function Pkg() {
   }
@@ -26,5 +28,39 @@ class Pkg {
   public function setName($val) {
     $this->_name = $val;
   }
+
+  /**
+   * @return mixed
+   */
+  public function getVersion()
+  {
+    return $this->_version;
+  }
+
+  /**
+   * @param mixed $version
+   */
+  public function setVersion($version)
+  {
+    $this->_version = $version;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getRelease()
+  {
+    return $this->_release;
+  }
+
+  /**
+   * @param mixed $release
+   */
+  public function setRelease($release)
+  {
+    $this->_release = $release;
+  }
+
+
 }
 ?>
---
+++ lib/modules/gui/www/test.php
@@ -0,0 +1,17 @@
+<?php
+include(realpath(dirname(__FILE__)) . '/../../../model/Pkg.php');
+include(realpath(dirname(__FILE__)) . '/../../../dao/PkgDao.php');
+include(realpath(dirname(__FILE__)) . '/../../../managers/DbManager.php');
+
+$pkg = new Pkg();
+$dbManager = new DbManager();
+
+$pkg->setName("test_pkg");
+$pkg->setRelease("release123");
+$pkg->setVersion("1.2.3");
+
+$pkgDao = new PkgDao($dbManager);
+
+$pkgDao->create($pkg);
+echo ('</br> done');
+?>
